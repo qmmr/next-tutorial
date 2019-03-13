2f1763c2b1e99802d263c3ec5c102cafecbd49a2
Marcin Kumorek <kumeek@gmail.com>
2019-03-13 16:02:16 +0100

    Add clean URLs to links :see_no_evil:

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Layout from '../components/Layout.js'
 import Link from 'next/link'
 
-const PostLink = ({ title }) => (
+const PostLink = ({ id, title }) => (
 	<li>
-		<Link href={`/post?title=${title}`}>
+		<Link as={`/p/${id}`} href={`/post?title=${title}`}>
 			<a>{title}</a>
 		</Link>
 	</li>
@@ -13,9 +13,9 @@ const Index = () => (
 	<Layout>
 		<h1>My Blog</h1>
 		<ul>
-			<PostLink title="Hello Next.js" />
-			<PostLink title="Learn Next.js is awesome" />
-			<PostLink title="Deploy apps with Zeit" />
+			<PostLink id="hello-nextjs" title="Hello Next.js" />
+			<PostLink id="nextjs-is-awesome" title="Learn Next.js is awesome" />
+			<PostLink id="deploy-with-zeit" title="Deploy apps with Zeit" />
 		</ul>
 	</Layout>
 )
