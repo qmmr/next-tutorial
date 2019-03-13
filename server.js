de2382732b5692d77db0dbce26aa85d451ba7384
Marcin Kumorek <kumeek@gmail.com>
2019-03-13 16:17:48 +0100

    Add server mapping to post pages :boom:

--- server.js
+++ server.js
@@ -10,12 +10,20 @@ app
 	.then(() => {
 		const server = express()
 
+		server.get('/p/:id', (req, res) => {
+			const actualPage = '/post'
+			const queryParams = { title: req.params.id }
+
+			app.render(req, res, actualPage, queryParams)
+		})
+
 		server.get('*', (req, res) => {
 			return handle(req, res)
 		})
 
 		server.listen(3000, err => {
 			if (err) throw err
+
 			console.log('> Ready on http://localhost:3000')
 		})
 	})
